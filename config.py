758e7be44dcb605a86d6d65713d8d3a73ff01f4a
xiong <aaxwhxwh@gmail.com>
2018-08-08 11:48:42 +0800

    new_project

---
+++ config.py
@@ -0,0 +1,28 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@author:will
+@file: config.py
+@time: 2018/08/08
+"""
+from redis import StrictRedis
+
+
+class Config(object):
+
+    SECRET_KEY = 'VwrDqe7gN8rDax4J7nSikmVaZbWA7wS1YVtdouJLLIRoK/xdYomdAw=='
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:mysql@127.0.0.1:3306/news_web'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    SESSION_TYPE = 'redis'
+    SESSION_USE_SIGNER = True
+    PERMANENT_SESSION_FILETIME = 86400
+    SESSION_REDIS = StrictRedis(host='127.0.0.1', port=6379)
+
+
+class Develop(Config):
+    DEBUG = True
+
+
+class Produce(Config):
+    pass
--- manage.py
+++ manage.py
@@ -6,15 +6,24 @@
 @time: 2018/08/08
 """
 
-from flask import Flask
+from flask import Flask, session
+from flask_script import Manager
+from config import Config, Develop, Produce
+from flask_sqlalchemy import SQLAlchemy
+from flask_session import Session
 
 app = Flask(__name__)
+app.config.from_object(Develop)
+Session(app)
+db = SQLAlchemy(app)
+manage = Manager(app)
 
 
 @app.route('/')
 def index():
+    session['name'] = 2412413
     return 'index'
 
 
 if __name__ == "__main__":
-    app.run()
+    manage.run()
